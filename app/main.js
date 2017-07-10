2c695efc99fb2999d92abc51a8a7ff042a295b09
Jesse Hill <jsnahill@gmail.com>
2017-07-10 13:55:48 -0500

    Create new windows

--- app/main.js
+++ app/main.js
@@ -1,17 +1,47 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
-let mainWindow = null;
+const windows = new Set();
+
+const createWindow = exports.createWindow = () => {
+  let newWindow = new BrowserWindow({ show: false });
+  windows.add(newWindow);
+
+  newWindow.loadURL(`file://${__dirname}/index.html`);
+
+  newWindow.once('ready-to-show', () => {
+    newWindow.show();
+  });
+
+  newWindow.on('closed', () => {
+    windows.delete(newWindow);
+    newWindow = null;
+  });
+};
+
+const getFileFromUserSelection = exports.getFileFromUserSelection = (targetWindow) => {
+  const files = dialog.showOpenDialog(targetWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'text'] },
+      { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+    ]
+  });
+
+  if (!files) {
+    return;
+  }
+
+  return files[0];
+};
+
+const openFile = exports.openFile = (targetWindow, filePath) => {
+  const file = filePath || getFileFromUserSelection(targetWindow);
+  const content = fs.readFileSync(file).toString();
+
+  targetWindow.webContents.send('file-opened', file, content);
+};
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
-
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+  createWindow();
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,4 +1,7 @@
 const marked = require('marked');
+const { remote, ipcRenderer } = require('electron');
+const mainProcess = remote.require('./main');
+const currentWidnow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -7,3 +10,25 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+
+const renderMarkdownToHtml = (markdown) => {
+  const html = marked(markdown, { sanitize: true });
+  htmlView.innerHTML = html;
+};
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdownToHtml(event.target.value);
+});
+
+openFileButton.addEventListener('click', (event) => {
+  mainProcess.openFile(currentWidnow);
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+});
+
+newFileButton.addEventListener('click', (event) => {
+  mainProcess.createWindow();
+})
