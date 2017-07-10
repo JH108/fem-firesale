877d8fe20d73484356691adf0cf3b86033db5cec
Jesse Hill <jsnahill@gmail.com>
2017-07-10 16:46:12 -0500

    Finish fire sale text editor

--- app/main.js
+++ app/main.js
@@ -2,19 +2,45 @@ const { app, BrowserWindow, dialog } = require('electron');
 const fs = require('fs');
 
 const windows = new Set();
+const fileWatchers = new Map();
 
-const createWindow = exports.createWindow = () => {
+const createWindow = exports.createWindow = (file) => {
   let newWindow = new BrowserWindow({ show: false });
   windows.add(newWindow);
 
   newWindow.loadURL(`file://${__dirname}/index.html`);
 
   newWindow.once('ready-to-show', () => {
+    if (file) {
+      openFile(newWindow, file);
+    }
     newWindow.show();
+  });
+
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      event.preventDefault();
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: 'Quit with Unsaved Changes?',
+        message: 'Your changes will be lost if you do not save first.',
+        buttons: [
+          'Quit Anyway',
+          'Cancel'
+        ],
+        defaultId: 0,
+        cancelId: 1
+      });
+
+      if (result === 0) {
+        newWindow.destroy();
+      }
+    }
   });
 
   newWindow.on('closed', () => {
     windows.delete(newWindow);
+    stopWatching(newWindow);
     newWindow = null;
   });
 };
@@ -39,8 +65,57 @@ const openFile = exports.openFile = (targetWindow, filePath) => {
   const file = filePath || getFileFromUserSelection(targetWindow);
   const content = fs.readFileSync(file).toString();
 
+  app.addRecentDocument(file);
+  startWatching(targetWindow, file);
+
+  targetWindow.webContents.send('file-opened', file, content);
+  targetWindow.setRepresentedFilename(file);
+};
+
+const saveMarkdown = exports.saveMarkdown = (targetWindow, file, content) => {
+  if (!file) {
+    file = dialog.showSaveDialog(targetWindow, {
+      title: 'Save Markdown',
+      defaultPath: app.getPath('documents'),
+      filters: [
+        { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+      ]
+    });
+  }
+
+  if (!file) {
+    return;
+  }
+
+  fs.writeFileSync(file, content).toString();
   targetWindow.webContents.send('file-opened', file, content);
 };
+
+const startWatching = (targetWindow, file) => {
+  stopWatching(targetWindow);
+
+  const watcher = fs.watch(file, (event) => {
+    if (event === 'change') {
+      const content = fs.readFileSync(file);
+      targetWindow.webContents.send('file-changed', file, content);
+    }
+  });
+
+  fileWatchers.set(targetWindow, watcher);
+};
+
+const stopWatching = (targetWindow) => {
+  if (fileWatchers.has(targetWindow)) {
+    fileWatchers.get(targetWindow).close();
+    openFiles.delete(targetWindow);
+  }
+};
+
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, filePath) => {
+    createWindow(filePath);
+  });
+});
 
 app.on('ready', () => {
   createWindow();
--- app/renderer.js
+++ app/renderer.js
@@ -1,7 +1,7 @@
 const marked = require('marked');
-const { remote, ipcRenderer } = require('electron');
+const { remote, ipcRenderer, shell } = require('electron');
 const mainProcess = remote.require('./main');
-const currentWidnow = remote.getCurrentWindow();
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -10,25 +10,70 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+let filePath = null;
+let originalContent = '';
 
 const renderMarkdownToHtml = (markdown) => {
   const html = marked(markdown, { sanitize: true });
   htmlView.innerHTML = html;
 };
 
+const updateEditedState = (isEdited) => {
+  currentWindow.setDocumentEdited(isEdited);
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  let title = 'Fire Sale';
+  if (filePath) title = `${filePath} - ${title}`;
+  if (isEdited) title = `${title} (Edited)`;
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', (event) => {
+  const currentContent = event.target.value;
+
   renderMarkdownToHtml(event.target.value);
+  updateEditedState(originalContent !== currentContent);
 });
 
 openFileButton.addEventListener('click', (event) => {
-  mainProcess.openFile(currentWidnow);
+  mainProcess.openFile(currentWindow);
+});
+
+saveMarkdownButton.addEventListener('click', () => {
+  mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value);
+});
+
+showFileButton.addEventListener('click', () => {
+  shell.showItemInFolder(filePath);
+});
+
+openInDefaultButton.addEventListener('click', () => {
+  shell.openItem(filePath);
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
   markdownView.value = content;
   renderMarkdownToHtml(content);
+  updateEditedState(false);
 });
 
 newFileButton.addEventListener('click', (event) => {
   mainProcess.createWindow();
-})
+});
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+  updateEditedState(false);
+});
